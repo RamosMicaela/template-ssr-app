99dc9dda5ff7ae4f1216f7991e42351a9f71bea4
Micaela Ramos <micaela.ramos@mercadolibre.com>
2021-01-18 18:41:47 -0300

    Organize render middleware

--- server/index.js
+++ server/index.js
@@ -1,30 +1,11 @@
-import path from 'path';
-import fs from 'fs';
-
-import React from 'react';
 import express from 'express';
-import ReactDOMServer from 'react-dom/server';
 
-import App from '../src/App';
+import render from '../middlewares/render/index.js';
 
 const PORT = process.env.PORT || 3006;
 const app = express();
 
-app.get('/', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App />);
-
-  const indexFile = path.resolve('./build/index.html');
-  fs.readFile(indexFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Something went wrong:', err);
-      return res.status(500).send('Oops, better luck next time!');
-    }
-
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    );
-  });
-});
+app.get('/', render);
 
 app.use(express.static('./build'));
 
